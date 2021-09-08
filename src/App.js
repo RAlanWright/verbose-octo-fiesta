8869d76b26caa0d8d93d914fcd536375a3fb464f
Alan <wright.alan88@gmail.com>
2021-09-08 18:21:24 -0500

    Generated Pet component and added script tag linking to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+const Pet = () => {
+	return React.createElement('div', {}, [
+		React.createElement('h2', {}, 'Conan'),
+		React.createElement('h3', {}, 'Cat'),
+		React.createElement('h3', {}, 'Russian Blue'),
+	]);
+};
+
 const App = () => {
-	return React.createElement(
-		'div',
-		{},
+	return React.createElement('div', {}, [
 		React.createElement('h1', {}, 'Adopt Me!'),
-	);
+		React.createElement(Pet),
+		React.createElement(Pet),
+		React.createElement(Pet),
+	]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
