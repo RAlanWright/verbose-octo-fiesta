82fd75fd2e36eb77f8f34ee18a8e35f09761de41
Alan <wright.alan88@gmail.com>
2021-09-08 18:37:25 -0500

    Added props to Pet component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-const Pet = () => {
+const Pet = (props) => {
 	return React.createElement('div', {}, [
-		React.createElement('h2', {}, 'Conan'),
-		React.createElement('h3', {}, 'Cat'),
-		React.createElement('h3', {}, 'Russian Blue'),
+		React.createElement('h2', {}, props.name),
+		React.createElement('h3', {}, props.animal),
+		React.createElement('h3', {}, props.breed),
 	]);
 };
 
 const App = () => {
 	return React.createElement('div', {}, [
-		React.createElement('h1', {}, 'Adopt Me!'),
-		React.createElement(Pet),
-		React.createElement(Pet),
-		React.createElement(Pet),
+		React.createElement('h1', { id: 'my-brand' }, 'Adopt Me!'),
+		React.createElement(Pet, {
+			name: 'Conan',
+			animal: 'Cat',
+			breed: 'Russian Blue',
+		}),
+		React.createElement(Pet, {
+			name: 'Alpha',
+			animal: 'Cat',
+			breed: 'Maine Coon',
+		}),
+		React.createElement(Pet, {
+			name: 'Leo',
+			animal: 'Cat',
+			breed: 'Domestic Long Hair',
+		}),
+		React.createElement(Pet, {
+			name: 'Spencer',
+			animal: 'Dog',
+			breed: 'Deer Chihuahua',
+		}),
 	]);
 };
 
